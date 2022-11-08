cba04b0b7340671e8cbcef471dc1740862132241
Miguel Gavilán <miangame1@gmail.com>
2022-11-08 08:00:55 +0100

    rename file

--- tm-infra-commands.user.js
+++ tm-infra-commands.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Infra commands
-// @namespace    http://tampermonkey.net/
+// @namespace    Genially scripts
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Miguel Á. Gavilán Merino
 // @match        https://github.com/Genially/mono-genially/pull/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @downloadURL  https://gist.githubusercontent.com/Miangame/8fd5833c1d431d3ea9d0bb6d49f18191/raw/e3cc2a55b7b9aafd461c6c1e034cb6feaac7d5f8/tm-infra-commands.user.js
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
@@ -27,33 +28,39 @@
   const buttons = [
     {
       text: "Bazinga",
+      description: "Recreates the ED",
       command: commands.BAZINGA,
     },
     {
       text: "Dracarys",
+      description: "Destroys the ED",
       command: commands.DRACARYS,
     },
     {
       text: "Emilio patronus",
+      description: "Activates the emails in the ED",
       command: commands.EMILIO_PATRONUS,
     },
     {
       text: "Ed reparo",
+      description: "Restarts the ED without compiling it",
       command: commands.ED_REPARO,
     },
     {
       text: "Lumos",
+      description: "Activates the ED",
       command: commands.LUMOS,
     },
   ];
 
-  const createButton = (buttonText, callback) => {
+  const createButton = (buttonText, description, callback) => {
     const button = document.createElement("button");
     button.classList = "btn btn-danger";
     button.type = "button";
     button.style.marginTop = "10px";
     button.style.marginRight = "10px";
     button.innerText = buttonText;
+    button.title = description;
     button.onclick = callback;
 
     return button;
@@ -80,14 +87,17 @@
     );
 
     const newButtonsContainer = document.createElement("div");
+    newButtonsContainer.id = "infra-commands";
     newButtonsContainer.style.display = "flex";
 
-    if (buttonsContainer) {
+    const existsNewButtonsContainer = document.getElementById("infra-commands");
+
+    if (buttonsContainer && !existsNewButtonsContainer) {
       buttonsContainer.appendChild(newButtonsContainer);
 
-      buttons.forEach(({ text, command }) => {
+      buttons.forEach(({ text, description, command }) => {
         newButtonsContainer.appendChild(
-          createButton(text, () => launchCommand(command))
+          createButton(text, description, () => launchCommand(command))
         );
       });
     }
